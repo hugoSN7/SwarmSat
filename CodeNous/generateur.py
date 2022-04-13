30bfbf8ecacb7d09786cd2fb8f308b55813fefa2
reveghis <75759598+reveghis@users.noreply.github.com>
2022-04-13 17:20:31 +0200

    correction + redimensionnement

--- CodeNous/generateur.py
+++ CodeNous/generateur.py
@@ -1,4 +1,5 @@
 from array import array
+from telnetlib import theNULL
 import numpy as np
 
 with open("Traces.csv") as file:
@@ -15,7 +16,9 @@ cols = len(tab[0])
 for i in range (0,rows-1, 3):
     fichier.write("LINESTRING (")
     for j in range(0,cols-1):
-        fichier.write(str(tab[i,j])+" "+str(tab[i+1,j])+", ")
+        fichier.write(str(tab[i,j])+" "+str(tab[i+2,j]))
+        if (j<cols-1):
+            fichier.write(", ")
     fichier.write(")\n\n")
 fichier.close()
 
