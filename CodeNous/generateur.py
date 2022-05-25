87159f2795be96272be0da87937285a168068f68
hugoSN7 <hugo.casuzanne@gmail.com>
2022-05-25 16:32:48 +0200

    ajout Test.csv

--- CodeNous/generateur.py
+++ CodeNous/generateur.py
@@ -1,15 +1,17 @@
 from array import array
 from telnetlib import theNULL
 import numpy as np
+import csv
 
-with open("Traces.csv") as file:
+with open("Test.csv") as file:
     tab= np.loadtxt(file,delimiter=",")
 
+fichier = open("/home/ternardin/Documents/2A/SwarmSat/the-one-1.6.0/data/Ltrajectoire.wkt", 'w')
 
-fichier = open("trajectoire.wkt", 'w')
+
 #fichier.close()
 
-#with open("trajectoire.wkt") as file:
+#with open("Ltrajectoire.wkt") as file:
 rows = len(tab)
 cols = len(tab[0])
 
@@ -17,10 +19,12 @@ for i in range (0,rows-1, 3):
     fichier.write("LINESTRING (")
     for j in range(0,cols-1):
         fichier.write(str(tab[i,j])+" "+str(tab[i+2,j]))
-        if (j<cols-1):
+        if (j<cols-2):
             fichier.write(", ")
     fichier.write(")\n\n")
 fichier.close()
+
+
 
 
 
